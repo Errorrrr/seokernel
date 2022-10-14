64ab513784aaf1381b4bdc91e689fb2ad9290005
danil <dyakunin@sfedu.ru>
2022-10-14 04:14:06 +0300

    test

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Telegram\Bot\Api;
+
+class TelegramController extends Controller
+{
+    public function webhook(){
+        $telegram = new Api(env('TELEGRAM_API'));
+        $requ= json_decode(file_get_contents('php://input'),true);
+        \Illuminate\Support\Facades\Log::debug($requ);
+
+        $userid = $requ['message']['from']['id'];
+        $text   = $requ['message']['text'];
+
+
+        if ($text == '/start') {
+            $user = User::where('name','=',$requ['message']['from']['username'])->first();
+            if($user == null){
+                $response = $telegram->sendMessage([
+                    'chat_id' => $userid,
+                    'text' => 'Вы незарегистрированы',
+                ]);
+            }else{
+                $pass = Str::random(rand(8,12));
+                $user->forceFill([
+                    'password' => Hash::make($pass)
+                ])->setRememberToken(Str::random(60));
+                $user->save();
+
+                $response = $telegram->sendMessage([
+                    'chat_id' => $userid,
+                    'text' => 'Ваш пароль: '.$pass.' Продолжите на сайте: https://turbo-yadro.ru/login',
+                ]);
+
+            }
+
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -52,12 +52,7 @@ Route::get('api/get_stops', 'SettingsController@getStops')->middleware(['auth'])
 Route::post('api/change_price', 'SettingsController@changePrice')->middleware(['auth']);
 Route::post('api/change_stops', 'SettingsController@changeStops')->middleware(['auth']);
 Route::post('api/change_password', 'SettingsController@changePass')->middleware(['auth']);
-Route::post('/Tqw23ET75kytVNZEUREI84/bot_webhook', function () {
-    \Illuminate\Support\Facades\Log::debug('An informational message.');
-
-    $Bot = new \SimpleBotAPI\TelegramBot(env('TELEGRAM_API'), new \App\Handlers\BotHandler());
-    $Bot->OnWebhookUpdate();
-});
+Route::post('/Tqw23ET75kytVNZEUREI84/bot_webhook', 'TelegramController@webhook');
 
 Route::get('/settings', 'SettingsController@index')->middleware(['auth']);
 
