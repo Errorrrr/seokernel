eec0febab70db39f0b2e5d183636cf3c0c7a2d8c
danil <dyakunin@sfedu.ru>
2022-10-18 16:21:30 +0300

    test

--- app/Console/Commands/xmlThreadCommand.php
+++ app/Console/Commands/xmlThreadCommand.php
@@ -94,7 +94,7 @@ class xmlThreadCommand extends Command
         if($jsonInfo['cur_cost'] > 7){
             $xs_key = 'https://xmlstock.com/yandex/xml/?user=9455&key='.env('XMLSTACK_API_KEY');
         }else{
-            $xs_key = 'http://xmlproxy.ru/search/xml?page=1&user=omi4sem%40mail.ru&key='.env('XMLPROXY_API_KEY');
+            $xs_key = 'http://xmlproxy.ru/search/xml?groupby=attr%3Dd.mode%3Ddeep.groups-on-page%3D5.docs-in-group%3D3&maxpassages=3&page=1&user=omi4sem%40mail.ru&key='.env('XMLPROXY_API_KEY');
         }
 
         $query = ( '&query='.urlencode($query) ) . ( $region ? '&lr=' . urlencode($region) : '' );
