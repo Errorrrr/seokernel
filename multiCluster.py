c26a8de66afebb969fd49cce56bee8702915545d
danil <dyakunin@sfedu.ru>
2022-10-21 22:16:59 +0300

    test

--- multiCluster.py
+++ multiCluster.py
@@ -111,7 +111,7 @@ class Posts:
 
 def getThreadsList():
     result = []
-    for item in range(0,10):
+    for item in range(0,100):
         result.append(item)
     return result
 
@@ -138,7 +138,7 @@ processes = Processes()
 i = 0
 while(isWorking):
     i += 1
-    time.sleep(0.01)
+    time.sleep(0.1)
     if not threads.getFreeThread() is None and not posts.getPostNum() is None:
         postNum = posts.getPostNum()
         processes.startNewProcess(postNum, threads.getFreeThread())
@@ -149,7 +149,7 @@ while(isWorking):
     if not processes.hasWorkingProcesses() and posts.getPostNum() is None:
         print("Нет активных процессов, нет неразобранных запросов!")
         isWorking = False
-    if i == 100:
+    if i == 10:
         i = 0
         first, second, third = argv
         subprocess.Popen(str(second)+' '+os.path.dirname(os.path.realpath(__file__))+'/artisan update:progress ' + str(postNum), executable='/bin/bash', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
